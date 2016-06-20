3f249d4d57bf222bed13131946bbad5c4027655f
minibrary <minibrary@gmail.com>
2016-06-20 10:52:17 +0900

    Create docker-cusom.sh

---
+++ archlinux/private/docker-cusom.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Install Necessary Packages #
+pacman -Sy git npm nodejs base-devel nano ruby --noconfirm
+
+# Install Jekyll #
+gem install jekyll
+
+# Jekyll Symlink to $PATH #
+ln -s ~/.gem/ruby/2.3.0/bin/jekyll /usr/bin
+
+# Install Global gulp #
+npm install -g gulp
+
+# Make git local base directory #
+mkdir ~/git
+
+curl -O https://raw.githubusercontent.com/minibrary/docker/master/archlinux/private/git-pull.sh && chmod 755 git-pull.sh && ./git-pull.sh
+
+#Github Username
+user=minibrary
+
+#Github local base Base Directory
+basedir=~/git
+
+#Github Repositories
+repo01=kr.blog
+repo02=en.blog
+repo03=jp.blog
+repo04=docker
+#repo05=
+#repo06=
+#repo07=
+#repo08=
+#repo09=
+#repo10=
+
+# Install npm for each repo #
+for repo in $repo01 $repo02 $repo03;
+	do (cd $basedir/$repo && npm install)
+done
